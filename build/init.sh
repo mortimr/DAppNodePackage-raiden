5950ce92625d54a49a198de93ad11f40f49b7bb4
Iulian Rotaru <iulian@rotaru.fr>
2021-08-10 15:42:40 +0200

    feat: add support for custom eth rpc

--- build/init.sh
+++ build/init.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Check if password is set, and at least one file has been uploaded
-if [ -n "${RAIDEN_ADDRESS}" ] && [ -n "${RAIDEN_KEYSTORE_PASSWORD}" ] && [ "$(find . -maxdepth 1 -type f | wc -l)" -gt 0 ]; then
+if [ -n "${RAIDEN_ADDRESS}" ] && [ -n "${RAIDEN_KEYSTORE_PASSWORD}" ] && [ -n "${RAIDEN_ETH_RPC_ENDPOINT}" ] && [ "$(find . -maxdepth 1 -type f | wc -l)" -gt 0 ]; then
     echo "${RAIDEN_KEYSTORE_PASSWORD}" > .password
     raiden --keystore-path . --accept-disclaimer --password-file .password "${EXTRA_OPTS}"
 else
@@ -12,8 +12,9 @@ else
     echo "## Please follow the following steps:                 ##"
     echo "## 1: Upload a keystore file to /root/.raiden         ##"
     echo "## 2: Set the RAIDEN_ADDRESS of the keystore          ##"
-    echo "## 2: Set the RAIDEN_KEYSTORE_PASSWORD to open it     ##"
-    echo "## 3: Restart the package                             ##"
+    echo "## 3: Set the RAIDEN_KEYSTORE_PASSWORD to open it     ##"
+    echo "## 4: Set the RAIDEN_ETH_RPC_ENDPOINT to use          ##"
+    echo "## 5: Restart the package                             ##"
     echo "########################################################"
     while true; do sleep 5; done
 fi
