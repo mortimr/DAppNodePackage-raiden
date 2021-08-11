1c1b88121558a0759aaf84984bac36c6b358575e
Iulian Rotaru <iulian@rotaru.fr>
2021-08-11 11:49:59 +0200

    feat: build custom image and launch script

---
+++ build/init.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+# Check if password is set, and at least one file has been uploaded
+if [ -n "${RAIDEN_ADDRESS}" ] && [ -n "${RAIDEN_KEYSTORE_PASSWORD}" ] && [ "$(find . -maxdepth 1 -type f | wc -l)" -gt 0 ]; then
+    echo "${RAIDEN_KEYSTORE_PASSWORD}" > .password
+    raiden --keystore-path . --password-file .password "${RAIDEN_EXTRA_OPTS}"
+else
+    echo
+    echo "########################################################"
+    echo "## Raiden not configured to start!                    ##"
+    echo "## Please follow the following steps:                 ##"
+    echo "## 1: Upload a keystore file to /root/.raiden         ##"
+    echo "## 2: Set the RAIDEN_ADDRESS of the keystore          ##"
+    echo "## 3: Set the RAIDEN_KEYSTORE_PASSWORD to open it     ##"
+    echo "## 4: Restart the package                             ##"
+    echo "########################################################"
+    while true; do sleep 5; done
+fi
